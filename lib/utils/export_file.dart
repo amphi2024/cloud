8ee748f331e0d39e9996e7af71548dbb73f32b63
siar1234 <siar019283@gmail.com>
2026-04-08 21:55:31 +0900

    fix: ensure file export functionality works correctly on mobile

--- lib/utils/export_file.dart
+++ lib/utils/export_file.dart
@@ -7,34 +7,38 @@ import 'package:cloud/providers/transfers_provider.dart';
 import 'package:cloud/utils/toast.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../channels/app_web_channel.dart';
 
 Future<void> exportFile({required FileModel fileModel, required BuildContext context, required WidgetRef ref}) async {
-  final filePath = await FilePicker.platform.saveFile(
-      fileName: fileModel.name,
-      bytes: Uint8List.fromList([])
-  );
-
-  if(filePath != null) {
     if(fileModel.isAvailableOffline) {
       final file = File(fileModel.offlinePath);
-      await file.copy(filePath);
+      await FilePicker.platform.saveFile(
+          fileName: fileModel.name,
+          bytes: await file.readAsBytes()
+      );
+
       if(context.mounted) {
         showToast(context, AppLocalizations.of(context).get("export_successful"));
       }
     }
     else {
-      appWebChannel.downloadFileFromCloud(id: fileModel.id, filePath: filePath, onSuccess: () async {
+      appWebChannel.downloadFileFromCloud(id: fileModel.id, filePath: fileModel.temporaryPath, onSuccess: () async {
         ref.read(transfersProvider.notifier).removeItem(fileModel.id);
+
+        final file = File(fileModel.temporaryPath);
+        await FilePicker.platform.saveFile(
+            fileName: fileModel.name,
+            bytes: await file.readAsBytes()
+        );
+
         if(context.mounted) {
           showToast(context, AppLocalizations.of(context).get("export_successful"));
         }
       }, onProgress: (transferred, total) {
         ref.read(transfersProvider.notifier).insertItem(TransferState(fileId: fileModel.id, transferredBytes: transferred, totalBytes: total));
       });
+
     }
-  }
 }
