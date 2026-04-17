b736e6e3acd11880c707f5959b7bf2d1d0368042
siar1234 <siar019283@gmail.com>
2026-04-17 10:05:05 +0900

    fix: set offline path when initializing file model

--- lib/models/file_model.dart
+++ lib/models/file_model.dart
@@ -24,6 +24,7 @@ class FileModel {
 
   FileModel({required String id, Map<String, dynamic>? data}) : data = data ?? {}, _id = id {
     if(id.length > 2) {
+      offlinePath = PathUtils.join(appStorage.selectedUser.storagePath, "files", _id, _id, _id, name);
       isAvailableOffline = File(offlinePath).existsSync();
     }
   }
